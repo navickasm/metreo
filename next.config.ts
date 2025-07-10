6bc994282d6e3fb7ba233a14325bdc73614edafa
Mack Navickas <mjnavickas@gmail.com>
2025-07-10 13:43:57 -0500

    Prepared for deployment on gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
