948bc5d26ed384f2041c1905224dea92f412a153
Mack Navickas <mjnavickas@gmail.com>
2025-07-10 14:12:41 -0500

    Update next.config.ts for gh action

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: 'dist',
+    basePath: "/metreo",
     images: {
         unoptimized: true,
     },
