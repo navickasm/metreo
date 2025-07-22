b1c7d3f8d5380498b8ef5dce7b04efb330a650e6
Mack Navickas <mjnavickas@gmail.com>
2025-07-22 17:48:53 -0500

    Made next work locally w/ basePath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: 'dist',
-    basePath: "/metreo",
+    basePath: process.env.NODE_ENV === 'production' ? '/metreo' : '',
     images: {
         unoptimized: true,
     },
